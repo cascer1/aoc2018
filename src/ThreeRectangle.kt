cf4b5c4031867b3886671d02be7ce473b0c8cb35
Cas Eliëns <cas.eliens@isaac.nl>
2018-12-03 12:47:18 +0100

    remove unused hasOverlap field from ThreeRectangle

--- src/ThreeRectangle.kt
+++ src/ThreeRectangle.kt
@@ -1,3 +1,1 @@
-class ThreeRectangle(var id: Int, var x: Int, var y: Int, var width: Int, var height: Int) {
-    var hasOverlap: Boolean = false
-}
+class ThreeRectangle(var id: Int, var x: Int, var y: Int, var width: Int, var height: Int)
