cc564c2442786c58e0dfffe4d5cee743c4a4535a
Cas Eliëns <cas.eliens@isaac.nl>
2018-12-03 12:46:56 +0100

    store claim overlap status in separate array

--- src/Three.kt
+++ src/Three.kt
@@ -5,9 +5,11 @@ object Three {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val answer = findAnswerOne()
+        val answerOne = findAnswerOne()
+        val answerTwo = findAnswerTwo()
         println()
-        println("answer: \t\t\t$answer")
+        println("answer 1: \t\t\t$answerOne")
+        println("answer 2: \t\t\t$answerTwo")
     }
 
     private fun loadFile(): ArrayList<ThreeRectangle> {
@@ -44,18 +46,21 @@ object Three {
         return ThreeRectangle(rectangleId, rectangleStartX, rectangleStartY, rectangleWidth, rectangleHeight)
     }
 
-    private fun buildClaimMap(rectangles: ArrayList<ThreeRectangle>): Array<Array<String>> {
-        val squares = Array(1000) { Array(1000){ "." }  }
+    private fun buildClaimMap(
+        rectangles: ArrayList<ThreeRectangle>,
+        claims: Array<Boolean> = Array(0) { false }
+    ): Array<Array<String>> {
+        val squares = Array(1000) { Array(1000) { "." } }
 
         for (rectangle in rectangles) {
-            (rectangle.x until rectangle.x + rectangle.width).forEach{x ->
-                (rectangle.y until rectangle.y + rectangle.height).forEach{y ->
+            (rectangle.x until rectangle.x + rectangle.width).forEach { x ->
+                (rectangle.y until rectangle.y + rectangle.height).forEach { y ->
                     when {
                         squares[x][y] == "." -> squares[x][y] = "${rectangle.id}"
                         else -> {
                             val squareClaims = ArrayList<String>(squares[x][y].split(",").toList())
                             squareClaims.add(rectangle.id.toString())
-                            setClaimed(rectangles, squareClaims)
+                            setClaimed(claims, squareClaims)
                             squares[x][y] = squareClaims.joinToString(",")
                         }
                     }
@@ -66,21 +71,21 @@ object Three {
         return squares
     }
 
-    private fun setClaimed(rectangles: ArrayList<ThreeRectangle>, ids: List<String>) {
-        for(rectangle in rectangles) {
-            if(ids.contains(rectangle.id.toString())) {
-                rectangle.hasOverlap = true
-            }
+    private fun setClaimed(claims: Array<Boolean>, ids: List<String>) {
+        if (claims.isEmpty()) {
+            return
         }
+
+        ids.forEach { claims[it.toInt() - 1] = true }
     }
 
     private fun getOverlapCount(claimMap: Array<Array<String>>): Int {
         var overlaps = 0
 
-        (0 until claimMap.size).forEach{x ->
+        (0 until claimMap.size).forEach { x ->
             val y = claimMap[x]
-            (0 until y.size).forEach{
-                if(claimMap[x][it] != "." && claimMap[x][it].contains(",")) {
+            (0 until y.size).forEach {
+                if (claimMap[x][it] != "." && claimMap[x][it].contains(",")) {
                     overlaps++
                 }
             }
@@ -98,11 +103,12 @@ object Three {
 
     private fun findAnswerTwo(): Int {
         val rectangles = loadFile()
-        val claimMap = buildClaimMap(rectangles)
+        val claims = Array(rectangles.size) { false }
+        buildClaimMap(rectangles, claims)
 
-        for(rectangle in rectangles) {
-            if(!rectangle.hasOverlap) {
-                return rectangle.id
+        (0 until claims.size).forEach {
+            if (!claims[it]) {
+                return it + 1
             }
         }
 
